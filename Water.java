c1d8a48a689088b9e6ca58dcb79f98b4df572ea6
Jesvin-K-Justin <jesvinkjustin@gmail.com>
2024-11-25 19:32:02 +0530

    updated

--- Water.java
+++ Water.java
@@ -1,45 +1,68 @@
 
-import java.util.Arrays;
+import java.util.Scanner;
 
-interface WaterConservationSystem{
-    void calculateTrappedWater(int[] blockHeights);
+interface WaterConservationSystem
+{
+    public int calculateTrappedWater(int[] blockHeights);
 }
 
-abstract class RainySeasonConservation implements WaterConservationSystem {
+abstract class RainySeasonConservation implements WaterConservationSystem
+{
     
 }
 
-class CityBlockConservation extends RainySeasonConservation{
-
-     @Override
-     public void calculateTrappedWater(int[] blockHeights){
-        int WaterStored = 0;
-        int n=blockHeights.length;
-        for (int i = 1; i < n-1; i++) {
-            if (blockHeights[0]<blockHeights[n-1]) {
-                WaterStored += Math.abs(blockHeights[0] - blockHeights[i]);
-                
-            }
-            else{
-                WaterStored += Math.abs(blockHeights[n-1] - blockHeights[i]);
-            }
+class CityBlockConservation extends RainySeasonConservation
+{
+    @Override
+    public int calculateTrappedWater(int[] blockHeights)
+    {
+        if (blockHeights == null || blockHeights.length < 3) {
+            System.out.println("Insufficient blocks to trap water.");
+            return 0;
         }
-        System.out.println("Water Stored in the city block is: "+WaterStored);
-        
 
+        int n = blockHeights.length;
+        int[] leftMax = new int[n];
+        int[] rightMax = new int[n];
 
-     }
+        leftMax[0] = blockHeights[0];
+        for (int i = 1; i < n; i++) 
+            leftMax[i] = Math.max(leftMax[i - 1], blockHeights[i]);
+            
 
+        rightMax[n - 1] = blockHeights[n - 1];
+        for (int i = n - 2; i >= 0; i--) 
+            rightMax[i] = Math.max(rightMax[i + 1], blockHeights[i]);
+            
+
+        int trappedWater = 0;
+        for (int i = 0; i < n; i++) 
+            trappedWater += Math.min(leftMax[i], rightMax[i]) - blockHeights[i];
+            
+        return trappedWater;
+    }
 }
+
+
 public class Water {
     public static void main(String[] args) {
-        int[] blockHeights1 = {3,0,0,2,0,4};
-        int[] blockHeights2 = {3,0,2,0,4};
-        CityBlockConservation cityBlock = new CityBlockConservation();
-        System.out.println("City Blocks: "+Arrays.toString(blockHeights1));
-        cityBlock.calculateTrappedWater(blockHeights1);
-        System.out.println("City Blocks: "+Arrays.toString(blockHeights2));
-        cityBlock.calculateTrappedWater(blockHeights2);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the length of array : ");
+        int n = sc.nextInt();
+
+        System.out.print("\nEnter array : ");
+        int blockHeights[] = new int[n];
+
+        for(int i = 0; i < blockHeights.length; i++)
+        {
+           
+            blockHeights[i] = sc.nextInt();
+        }
+
+        CityBlockConservation A = new CityBlockConservation();
+        int trappedWater = A.calculateTrappedWater(blockHeights);
+        System.out.println("Total volume of trapped water: " + trappedWater + " units");
     }
     
 }
